62a93adb832b2c73363e4ae2fb9248f6dd841a20
Israel <israel.schmitt.j@gmail.com>
2020-09-16 14:51:09 -0300

    form refactor

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,5 +1,12 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, Validators } from '@angular/forms';
+import {
+  FormControl,
+  FormGroup,
+  Validators,
+  FormGroupDirective,
+  NgForm,
+  FormBuilder,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -9,24 +16,45 @@ import { FormControl, Validators } from '@angular/forms';
 export class LoginComponent implements OnInit {
   hide = true;
 
+  loginForm: FormGroup;
+  
   email = new FormControl('', [Validators.required, Validators.email]);
-
   password = new FormControl('', [Validators.required]);
+  confirmPassword = new FormControl('', [Validators.required]);
+  nome = new FormControl('', [Validators.required]);
+
+  checkPasswords(group: FormGroup) {
+    // here we have the 'passwords' group
+    let pass = group.get('password').value;
+    let confirmPass = group.get('confirmPass').value;
+
+    return pass === confirmPass ? null : { notSame: true };
+  }
 
   getEmailErrorMessage() {
     if (this.email.hasError('required')) {
       return 'Você precisa fornecer um email!';
     }
-
     return this.email.hasError('email') ? 'Email inválido!' : '';
   }
 
-  getPasswordErrorMessage(){
+  getPasswordErrorMessage() {
     if (this.password.hasError('required')) {
-      return 'Você precisa fornecer um password!';
+      return 'Você precisa fornecer uma senha!';
     }
   }
-  constructor() {}
+
+  getNomeErrorMessage() {
+    if (this.password.hasError('required')) {
+      return 'Você precisa fornecer um nome!';
+    }
+  }
+
+  constructor(private formBuilder: FormBuilder) {
+    this.loginForm = this.formBuilder.group({
+
+    })
+  }
 
   ngOnInit(): void {}
 }
