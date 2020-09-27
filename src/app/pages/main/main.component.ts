a11610a25a9f3ca4d7b64c6b6fa3f8bc441376c6
Israel <israel.schmitt.j@gmail.com>
2020-09-27 15:17:53 -0300

    table changes

--- src/app/pages/main/main.component.ts
+++ src/app/pages/main/main.component.ts
@@ -6,15 +6,36 @@ import { Item } from '../../services/item.model';
 import { MatPaginator } from '@angular/material/paginator';
 import { MatSort } from '@angular/material/sort';
 import { map } from 'rxjs/operators';
+import {
+  animate,
+  state,
+  style,
+  transition,
+  trigger,
+} from '@angular/animations';
 
 @Component({
   selector: 'app-main',
   templateUrl: './main.component.html',
   styleUrls: ['./main.component.css'],
+  animations: [
+    trigger('detailExpand', [
+      state('collapsed', style({ height: '0px', minHeight: '0' })),
+      state('expanded', style({ height: '*' })),
+      transition(
+        'expanded <=> collapsed',
+        animate('225ms cubic-bezier(0.4, 0.0, 0.2, 1)')
+      ),
+    ]),
+  ],
 })
 export class MainComponent implements OnInit {
   constructor(public auth: AuthService, private itensService: ItensService) {}
 
+  // dataSource2 = ELEMENT_DATA;
+  expandedElement: PeriodicElement | null;
+
+  isLoadingResults = true;
   dataSource: MatTableDataSource<Item>;
   // itens: Item[];
   @ViewChild(MatPaginator) paginator: MatPaginator;
@@ -29,18 +50,22 @@ export class MainComponent implements OnInit {
     'email_op',
   ];
 
+  columnsToDisplay = this.displayedColumns;
+
   applyFilter(event: Event) {
     const filterValue = (event.target as HTMLInputElement).value;
     this.dataSource.filter = filterValue.trim().toLowerCase();
   }
 
   ngOnInit(): void {
-    this.itensService.getItens().subscribe(async (itens: Item[]) => {
-      await itens.map((item, index) => {
+    this.itensService.getItens().subscribe((itens: Item[]) => {
+      itens.map((item, index) => {
         this.itensService.reverseGeocode(item.gps).subscribe((response) => {
           itens[index].gps = response.display_name;
+          this.isLoadingResults = false;
         });
       });
+
       this.dataSource = new MatTableDataSource(itens);
       this.dataSource.paginator = this.paginator;
       this.dataSource.sort = this.sort;
@@ -50,4 +75,13 @@ export class MainComponent implements OnInit {
     //   // console.log(response);
     // })
   }
+}
+
+export interface PeriodicElement {
+  id: string;
+  nome: string;
+  perfil: string;
+  gps: string;
+  validade: string;
+  email_op: string;
 }
